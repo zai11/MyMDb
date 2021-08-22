6db785888244801116ae68bf38f0d1c4881482fb
zai11 <zaipowell@rocketmail.com>
2021-08-22 20:59:41 +1000

    first commit

---
+++ MyMDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
